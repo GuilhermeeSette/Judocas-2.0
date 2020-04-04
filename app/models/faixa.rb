0ee6c76d00ecb6baed8469f53f37e88479b7a933
Guilherme Sette <guilhermesette@outlook.com>
2020-04-04 10:15:00 -0300

    create business rules
    Signed-off-by: Guilherme Sette <guilhermesette@outlook.com>

--- app/models/faixa.rb
+++ app/models/faixa.rb
@@ -1,3 +1,16 @@
 class Faixa < ApplicationRecord
   belongs_to :alunos, dependent: :destroy
+  COLORS = [
+    "BRANCA",
+    "CINZA",
+    "AZUL",
+    "AMARELA",
+    "LARANJA",
+    "VERDE",
+    "ROXA",
+    "MARROM",
+    "PRETA",
+    "CORAL",
+    "VERMELHA"
+  ].freeze
 end
