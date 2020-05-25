2859b6111aa9cc835de1a75a417889cf1845d657
Guilherme Sette <guilhermesette@outlook.com>
2020-05-25 13:58:03 -0300

    add method
    Signed-off-by: Guilherme Sette <guilhermesette@outlook.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :entidades
   resources :faixas
   resources :professors
-  resources :alunos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :alunos, path: 'alunos' do
+    collection do
+      match ":id/become_professor", as: "become", action: "become_professor", via: [:get]
+    end
+  end
 end
