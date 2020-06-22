6208e61fb95b0fdc45ae3b88324ca1ebd762556b
Guilherme Sette <guilhermesette@outlook.com>
2020-06-22 10:38:54 -0300

    adapt controllers
    Signed-off-by: Guilherme Sette <guilhermesette@outlook.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,4 @@
 class DashboardController < ApplicationController
   def show
-    list_of_alunos
-  end
-
-  private
-
-  def list_of_alunos
-    @alunos = ""
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,18 @@
 require 'simplecov'
-SimpleCov.start
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
+SimpleCov::Formatter::CoberturaFormatter])
+SimpleCov.start do
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Lib", "lib/"
+  add_group "Helpers", "app/helpers"
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
