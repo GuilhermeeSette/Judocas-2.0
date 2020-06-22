a619e30b0cd46e4b8972979eb8b34a5a48d2350f
Guilherme Sette <guilhermesette@outlook.com>
2020-06-22 10:39:51 -0300

    create tests
    Signed-off-by: Guilherme Sette <guilhermesette@outlook.com>

--- test/controllers/professors_controller_test.rb
+++ test/controllers/professors_controller_test.rb
@@ -9,17 +9,4 @@ class ProfessorsControllerTest < ActionDispatch::IntegrationTest
     get professors_url
     assert_response :success
   end
-
-  test "should get edit" do
-    get edit_professor_url(@professor)
-    assert_response :success
-  end
-
-  test "should destroy professor" do
-    assert_difference('Professor.count', -1) do
-      delete professor_url(@professor)
-    end
-
-    assert_redirected_to professors_url
-  end
 end
